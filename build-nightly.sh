12acb9b8b8dc4c2f4af10c28a0492b6259dee5f3
Fred Emmott <fe@fb.com>
2018-07-25 15:21:39 -0700

    Add scripts to build nightlies

---
+++ build-nightly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+git pull --rebase
+brew list | grep hhvm | xargs brew uninstall --force || true
+brew upgrade
+brew install --build-bottle ./hhvm-nightly.rb
+rm *.json || true
+brew bottle --force-core-tap --root-url=https://dl.hhvm.com/homebrew-bottles --json ./hhvm-nightly.rb
+aws s3 sync ./ s3://hhvm-downloads/homebrew-bottles/ --exclude '*' --include '*.bottle.tar.gz'
+git pull --rebase
+brew bottle --merge --write --no-commit *.json
+git add hhvm-nightly.rb
+git commit -m "update bottle for nightly on $(sw_vers -productVersion)"
+git push
+rm *.bottle.{tar,gz,json}
---
+++ update-nightly.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ex
+DATE=$(date +%Y.%m.%d)
+URL="https://dl.hhvm.com/source/nightlies/hhvm-nightly-${DATE}.tar.gz"
+
+MYTEMP=$(mktemp -d)
+pushd $MYTEMP
+wget "$URL"
+wget "$URL.sig"
+gpg --verify *.sig
+SHA="$(openssl sha -sha256 -r *.tar.gz | awk '{print $1}')"
+popd
+rm -rf "$MYTEMP"
+
+git pull --rebase
+# --dry-run: no git actions...
+# --write: ... but write to the local repo anyway
+brew bump-formula-pr \
+	--dry-run \
+	--write \
+	--url="${URL}" \
+	--sha256="${SHA}" \
+	./hhvm-nightly.rb
+git add hhvm-nightly.rb
+git commit -m 'Update nightly version'
+git push
