776c9e5e062cc997aac236f70e88e6b1ccdd3e7a
Fred Emmott <fred@fredemmott.co.uk>
2018-08-07 14:28:34 -0700

    Rename nightly bottles before uploading

--- build-nightly.sh
+++ build-nightly.sh
@@ -7,6 +7,11 @@ brew upgrade
 brew install --build-bottle ./hhvm-nightly.rb
 rm *.json || true
 brew bottle --force-core-tap --root-url=https://dl.hhvm.com/homebrew-bottles --json ./hhvm-nightly.rb
+# FIXME: Unsure what's intended here for now
+# See https://discourse.brew.sh/t/double-dash-bottle-naming-details/2712
+for file in *--*.bottle.tar.gz; do
+  mv "$file" "$(echo "$file" | sed s/--/-/)"
+done
 aws s3 sync ./ s3://hhvm-downloads/homebrew-bottles/ --exclude '*' --include '*.bottle.tar.gz'
 git pull --rebase
 brew bottle --merge --keep-old --write --no-commit *.json
