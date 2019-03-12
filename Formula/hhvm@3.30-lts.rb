f42e65948f8b48b7a32b86a27edf2d93f0ace86d
Fred Emmott <fe@fb.com>
2019-03-12 10:24:26 -0700

    Default to proxygen server, don't set log level to Warning
    The log level chagne crashes in server mode at the moment. That needs
    debugging, but lets' stop shipping a broken config by default for now
    
    fastcgi hasn't been recommended for a while, stop enablign it by
    default.

--- Formula/hhvm@3.30-lts.rb
+++ Formula/hhvm@3.30-lts.rb
@@ -244,7 +244,6 @@ EOF
       session.gc_maxlifetime = 1440
 
       ; hhvm specific
-      hhvm.log.level = Warning
       hhvm.log.always_log_unhandled_exceptions = true
       hhvm.log.runtime_error_reporting_level = 8191
       hhvm.mysql.typed_results = false
@@ -261,7 +260,6 @@ EOF
       ; hhvm specific
 
       hhvm.server.port = 9000
-      hhvm.server.type = fastcgi
       hhvm.server.default_document = index.php
       hhvm.log.use_log_file = true
       hhvm.log.file = #{var}/log/hhvm/error.log
