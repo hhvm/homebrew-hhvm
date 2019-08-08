065aaca25cf3e4234df985c8473e34651d8edf6b
Fred Emmott <fe@fb.com>
2019-08-08 13:04:40 -0700

    Unify nightly and release build scripts
    Current process:
    
    - update-nightly.sh is ran by one worker and updates the source versions, and pushes
    - both workers run build-nightly.sh, which builds, updates .rb file, resolves merge conflicts, and pushes
    
    New process:
    
    - both run NIGHTLY=true ./build-release.sh $VERSION which builds, updates .rb file resolves merge conflicts, and pushes

--- build-nightly.sh
+++ build-nightly.sh
@@ -2,29 +2,4 @@
 set -ex
 
 git pull --rebase
-brew list | grep hhvm | xargs brew uninstall --force || true
-brew upgrade
-cd Formula
-brew install --bottle-arch=nehalem --build-bottle ./hhvm-nightly.rb
-rm *.json || true
-brew bottle --force-core-tap --root-url=https://dl.hhvm.com/homebrew-bottles --json ./hhvm-nightly.rb
-# FIXME: Unsure what's intended here for now
-# See https://discourse.brew.sh/t/double-dash-bottle-naming-details/2712
-for file in *--*.bottle.tar.gz; do
-  mv "$file" "$(echo "$file" | sed s/--/-/)"
-done
-aws s3 sync ./ s3://hhvm-downloads/homebrew-bottles/ --exclude '*' --include '*.bottle.tar.gz'
-
-function commit_and_push_bottle() {
-  brew bottle --merge --keep-old --write --no-commit *.json
-  git add hhvm-nightly.rb
-  git commit -m "update bottle for nightly on $(sw_vers -productVersion)"
-  git push
-}
-
-if !(git pull --rebase && commit_and_push_bottle); then
-  git rebase --abort || true
-  git reset --hard origin/master
-  commit_and_push_bottle
-fi
-rm *.bottle.{tar.gz,json}
+NIGHTLY=true ./build-release $(date +%Y.m.%d)
