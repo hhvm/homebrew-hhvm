005739f2425ff2477ba95d6610c9d25af57e4346
Fred Emmott <fe@fb.com>
2019-08-08 15:30:45 -0700

    unlink md5sha1sum on azure macos

--- azure-build-release.sh
+++ azure-build-release.sh
@@ -2,6 +2,7 @@
 set -ex
 
 brew upgrade
+brew unlink md5sha1sum # conflicts with coreutils
 brew install gnu-sed coreutils
 brew tap hhvm/hhvm
 brew install $(brew deps --include-build hhvm-nightly)
