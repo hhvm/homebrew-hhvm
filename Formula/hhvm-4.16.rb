b6fcaaf8f74f05b7520579cf42d4b410f71a40c9
vsts <vsts@Mac-551.local>
2019-08-09 22:35:36 +0000

    Added bottle for 4.16.2 on 10.13.6

--- Formula/hhvm-4.16.rb
+++ Formula/hhvm-4.16.rb
@@ -10,6 +10,7 @@ class Hhvm416 < Formula
   bottle do
     root_url "https://dl.hhvm.com/homebrew-bottles"
     sha256 "56a8ae2b61c8943e0b4ae12d37d312b195fcfa7e6307c928cc6466bbe2203c6c" => :mojave
+    sha256 "dd6d94423b10a5cf86f9ea6f107b0d2f45fffa926013ca763711f8462df268b5" => :high_sierra
   end
 
   class << Hardware::CPU
