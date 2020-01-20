792ee88890b4049a09488c761a51bee8a1d4b051
HHVM Homebrew Bot (Azure) <opensource+hhvm-homebrew-bot@fb.com>
2020-01-20 04:07:05 +0000

    Added bottle for 2020.01.20 on 10.14.6

--- Formula/hhvm-nightly.rb
+++ Formula/hhvm-nightly.rb
@@ -13,6 +13,7 @@ class HhvmNightly < Formula
 
   bottle do
     root_url "https://dl.hhvm.com/homebrew-bottles"
+    sha256 "30038b15b81be2ab3b74e2daa9482195bac69438ffb3c5ffa74c2a51ca5e7211" => :mojave
     sha256 "ec7268c6356f7366c6e5539e0413a97d43274ddbcf1beb8e3ab9bc8dd2640446" => :high_sierra
   end
 
