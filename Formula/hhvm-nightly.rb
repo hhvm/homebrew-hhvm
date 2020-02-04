d611236149557157158d583e4042125c6489973c
HHVM Homebrew Bot (Azure) <opensource+hhvm-homebrew-bot@fb.com>
2020-02-04 03:07:35 +0000

    Updated hhvm-nightly.rb recipe to 2020.02.04

--- Formula/hhvm-nightly.rb
+++ Formula/hhvm-nightly.rb
@@ -8,13 +8,11 @@ class HhvmNightly < Formula
   desc "JIT compiler and runtime for the Hack language"
   homepage "http://hhvm.com/"
   head "https://github.com/facebook/hhvm.git"
-  url "https://dl.hhvm.com/source/nightlies/hhvm-nightly-2020.02.03.tar.gz"
-  sha256 "755ac26049d9e986d840c3e03cf60ca81ab3aa151a6c190f69ab7c6189d871c2"
+  url "https://dl.hhvm.com/source/nightlies/hhvm-nightly-2020.02.04.tar.gz"
+  sha256 "545d55cd2525573230625502019c03a258bd3e5a9f8e15980b1f23cf1a38e763"
 
   bottle do
     root_url "https://dl.hhvm.com/homebrew-bottles"
-    sha256 "08f96a9a0d733a391ac8e856879f886667ff2574162f4e231a487b32a9eae2f0" => :mojave
-    sha256 "2730b99a97afe761ad446b053cd48f058336d65b834e64e9a3de5ae8d8979f5e" => :high_sierra
   end
 
   class << Hardware::CPU
