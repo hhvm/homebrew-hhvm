14f51497cfa51b55e187701d27d0352aaeef9dfe
HHVM Homebrew Bot (Azure) <opensource+hhvm-homebrew-bot@fb.com>
2020-08-04 18:10:50 +0000

    Added bottle for 4.68.1 on 10.15.6

--- Formula/hhvm-4.68.rb
+++ Formula/hhvm-4.68.rb
@@ -13,6 +13,7 @@ class Hhvm468 < Formula
 
   bottle do
     root_url "https://dl.hhvm.com/homebrew-bottles"
+    sha256 "05a3a148f27ed735e945060cfe42c45788e13ad28f87b7c3455d9ed327fa8e18" => :catalina
     sha256 "ec9aecb3cee289673679c2ca73280ed21143c1301aacf086d227aff6d81fbcc2" => :mojave
   end
 
