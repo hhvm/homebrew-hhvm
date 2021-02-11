4704ee885ea0d2a08a8e6d75d65e6eb3fd90d28f
HHVM Homebrew Bot (Azure) <opensource+hhvm-homebrew-bot@fb.com>
2021-02-11 00:39:01 +0000

    Deleting stale bottles for 4.56.2

--- Formula/hhvm-4.56.rb
+++ Formula/hhvm-4.56.rb
@@ -14,7 +14,6 @@ class Hhvm456 < Formula
   bottle do
     root_url "https://dl.hhvm.com/homebrew-bottles"
     sha256 catalina: "acc718dae45fd406029400baee1a407e581b38bce4707ac9936cf69586bee9e3"
-    sha256 mojave: "a071088b5922d35b618e0dc37e9326e54910060c7da081957f85413959bae4a7"
   end
 
   option "with-debug", <<~EOS
