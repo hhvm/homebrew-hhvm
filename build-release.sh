c08e3f1037af54e0664722c52ff8ff4b0e2f3208
Fred Emmott <fe@fb.com>
2021-04-06 12:53:52 -0500

    Add --formula to avoid warning

--- build-release.sh
+++ build-release.sh
@@ -173,7 +173,7 @@ rm -f *.bottle *.json
 # build
 brew upgrade
 cd Formula
-brew install --bottle-arch=sandybridge --build-bottle "$(basename "$RECIPE")"
+brew install --bottle-arch=sandybridge --build-bottle --formula "$(basename "$RECIPE")"
 # Update the source-bump commit to reference dl.hhvm.com instead
 gsed -E -i 's,"file://.+/(hhvm-.+\.tar\.gz)"$,"'"${REAL_URL}"'",' "$RECIPE"
 git commit --amend "$RECIPE" --reuse-message HEAD
