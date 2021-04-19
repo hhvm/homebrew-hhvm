8f4efa6bb36aeff331b65e349c9571fd4869b8d4
HHVM Homebrew Bot (Azure) <opensource+hhvm-homebrew-bot@fb.com>
2021-04-19 19:08:15 +0000

    Updated hhvm-4.80.rb recipe to 4.80.4

--- Formula/hhvm-4.80.rb
+++ Formula/hhvm-4.80.rb
@@ -8,13 +8,11 @@ class Hhvm480 < Formula
   desc "JIT compiler and runtime for the Hack language"
   homepage "http://hhvm.com/"
   head "https://github.com/facebook/hhvm.git"
-  url "https://dl.hhvm.com/source/hhvm-4.80.3.tar.gz"
-  sha256 "efe3d8344320f56cfe51420f2b47adb1c627bb94d51348c90c375e247102b8e6"
+  url "https://dl.hhvm.com/source/hhvm-4.80.4.tar.gz"
+  sha256 "767c4ea2dc9aa136313436d51782edcca8f8503394962b2a6ded5f7b0e4b3e75"
 
   bottle do
     root_url "https://dl.hhvm.com/homebrew-bottles"
-    sha256 catalina: "b63a202398064a49b77e36fdd252662b351f4a982189649f86bb899934378cae"
-    sha256 mojave:   "546133c43087cc846c0a0b6d4dae535e476a9bf6e31176ec24762f9134427be9"
   end
 
   option "with-debug", <<~EOS
