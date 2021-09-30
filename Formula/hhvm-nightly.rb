535e3876a8106b7801299259ee4f4bf5bc1809d1
HHVM Homebrew Bot (Azure) <opensource+hhvm-homebrew-bot@fb.com>
2021-09-30 05:16:11 +0000

    Added bottle for 2021.09.30 on 10.15.7

--- Formula/hhvm-nightly.rb
+++ Formula/hhvm-nightly.rb
@@ -13,6 +13,7 @@ class HhvmNightly < Formula
 
   bottle do
     root_url "https://dl.hhvm.com/homebrew-bottles"
+    sha256 catalina: "498da7f74cdfc1dddd1eb57c78e13c456aa42047dfed42919da8d5443b42eaf1"
   end
 
   option "with-debug", <<~EOS
